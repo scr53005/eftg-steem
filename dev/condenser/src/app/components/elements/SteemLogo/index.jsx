703a146d967619d90e25cdc83ea35ad1a8ff6d6b
Pablo Staiano <1355981+pablomat@users.noreply.github.com>
2018-09-27 17:36:56 +0000

    Condenser code

---
+++ dev/condenser/src/app/components/elements/SteemLogo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SteemLogo = () => {
+    return (
+        <span className="logo">
+	    <embed type="image/svg+xml" src="https://x01.xyz/EFTG.svg" width="150" height="40" />
+        </span>
+    );
+};
+
+export default SteemLogo;
